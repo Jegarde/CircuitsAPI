360a0039414bdbb588642a822661db8465106c92
Jesse <jesse.tapani.nieminen@gmail.com>
2024-04-05 19:41:51 +0300

    Delete circuitsapi directory

--- circuitsapi/__init__.py
+++
@@ -1,4 +0,0 @@
-from .client import RoomConnection, UserConnection, Client, LoginCookie
-from .exceptions import RoomNotFound, UserNotFound
-from .recnetlogin import RecNetLogin
-from .helpers import run_length_encoding, run_length_decoding
